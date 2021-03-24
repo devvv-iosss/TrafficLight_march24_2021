de3a46d4522972c7dc0ceabcc3fd072f474247b8
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-03-24 16:04:51 +0300

    final ver

--- TrafficLight_march24_2021/ViewController.swift
+++ TrafficLight_march24_2021/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 64
+        yellowLightView.layer.cornerRadius = 64
+        greenLightView.layer.cornerRadius = 64
+        
+        startButton.layer.cornerRadius = 10
+
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        if startButton.titleLabel?.text == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+            redLightView.alpha = 1
+        } else if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+       }
+    }
 }
-
