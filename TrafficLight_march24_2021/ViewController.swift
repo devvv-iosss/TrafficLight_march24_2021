3a1cc620c25c790bd82712763bfa41513e45072d
ios_dev <futureiosdeveloper@gmail.com>
2021-05-22 11:59:03 +0300

    1 cmt

--- TrafficLight_march24_2021/ViewController.swift
+++ TrafficLight_march24_2021/ViewController.swift
@@ -16,34 +16,6 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        redLightView.layer.cornerRadius = 64
-        yellowLightView.layer.cornerRadius = 64
-        greenLightView.layer.cornerRadius = 64
-        
-        startButton.layer.cornerRadius = 10
-
-        redLightView.alpha = 0.3
-        yellowLightView.alpha = 0.3
-        greenLightView.alpha = 0.3
-    }
-    
-    @IBAction func startButtonPressed() {
-        if startButton.titleLabel?.text == "START" {
-            startButton.setTitle("NEXT", for: .normal)
-            redLightView.alpha = 1
-        } else if redLightView.alpha == 1 {
-            redLightView.alpha = 0.3
-            yellowLightView.alpha = 1
-        } else if yellowLightView.alpha == 1 {
-            yellowLightView.alpha = 0.3
-            greenLightView.alpha = 1
-        } else if greenLightView.alpha == 1 {
-            greenLightView.alpha = 0.3
-            redLightView.alpha = 1
-       }
-    }
+   
 }
  
